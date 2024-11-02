d92bb1726d789c4c94b8bf1b495f87e998bdcd11
mikidevs <mikis168@protonmail.com>
2024-11-02 19:31:19 +0200

    before migration

--- src/main.js
+++ src/main.js
@@ -41,18 +41,6 @@ function draw(currentTime, lastUpdateTime, prevPlayerPos, prevAlienTs) {
 
   const newPlayerPos = Player.calculatePosition(prevPlayerPos, keysPressed, canvas.width, delta);
   Player.render(newPlayerPos, ctx);
-  const bullet = spawnBullet(newPlayerPos, keysPressed);
-
-  if (bullet) {
-
-  }
-
-  const monitor = prevAlienTs[0].velocity.x > 0 ? prevAlienTs[prevAlienTs.length - 1] : prevAlienTs[0];
-
-  const newAlienT = R.pipe(
-    R.map(t => Alien.calculateTransform(t, monitor, canvas.width, delta)),
-    R.forEach(t => Alien.render(t, ctx))
-  )(prevAlienTs);
 
   requestAnimationFrame((nextTime) => draw(nextTime, currentTime, newPlayerPos, newAlienT));
 }
@@ -61,5 +49,21 @@ const initPlayerPos = Vector.create((canvas.width - Player.WIDTH) / 2, canvas.he
 
 const initAlienTs = Array.from({ length: 11 }, 
   (v, i) => Alien.create(Vector.create(i * 1.6 * Alien.SIZE, 50), Vector.create(Alien.SPEED, 0)));
+
+const entities = [
+  { position: initPlayerPos, calulate: Player.calculatePosition, render: Player.render },
+  { position: initAlienTs, 
+    calulatePos: (prevTs, delta) => {
+      const monitor = prevTs[0].velocity.x > 0 ? prevTs[prevTs.length - 1] : prevTs[0];
+      return R.map(t => Alien.calculateTransform(t, monitor, canvas.width, delta))
+    },
+    render: Alien.render
+  },
+];
+
+const renderPipe = R.reduce(
+  (acc, current, idx) => idx,
+  [],
+);
 
 requestAnimationFrame((currentTime) => draw(currentTime, 0, initPlayerPos, initAlienTs));
